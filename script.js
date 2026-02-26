d1357b3e7068e2ebee61bc96e29b888382c40d26
Liam Labell <thefallenspirit@outlook.com>
2026-02-26 06:09:52 -0800

    Add footer and improve testimonials

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 // @ts-check
-
 const testimonials = [
     {
-        author: 'Fallen',
-        position: 'Server Administrator',
-        message: 'Fyre is a super duper cool bot that I really love and it has good features, good commands, shit yes yes lol.'
-    },
-    {
-        author: 'Fallen 2',
-        position: 'Server Administrator',
-        message: 'Fyre 2 is a super duper cool bot that I really love and it has good features, good commands, shit yes yes lol.'
-    },
-    {
-        author: 'Fallen 3',
-        position: 'Server Administrator',
-        message: 'Fyre 3 is a super duper cool bot that I really love and it has good features, good commands, shit yes yes lol.'
+        message: "Fyre is a great bot. Really fun to use, lots of features, and tons of quality of life improvements for you and your server. I highly recommend this gorgeous bot.",
+        author: {
+            name: "Fallen",
+            avatar: "https://cdn.discordapp.com/avatars/738746238874419220/a_b8d713e5a47e2026fe3b3d4cc30215e5.gif",
+            position: "Server Administrator"
+        },
+        guild: {
+            name: "Fyre Hub",
+            icon: "https://cdn.discordapp.com/icons/1370922624397606952/d256f11d947495281740da772f62b8fa.webp"
+        }
     }
 ];
 
@@ -24,13 +20,27 @@ let intervalId = 0;
 function renderTestimonial(index = 0) {
     const testimonial = testimonials[index];
 
-    const authorElement = document.getElementById('testimonial-author');
+    const indexElement = document.getElementById('testimonial-index');
     const messageElement = document.getElementById('testimonial-message');
-    const positionElement = document.getElementById('testimonial-position');
 
-    if (authorElement) authorElement.textContent = testimonial.author;
+    if (indexElement) indexElement.textContent = `${index + 1}/${testimonials.length}`;
     if (messageElement) messageElement.textContent = `"${testimonial.message}"`;
-    if (positionElement) positionElement.textContent = testimonial.position;
+
+    const guildIcon = document.getElementById('testimonial-guild-icon');
+    const guildName = document.getElementById('testimonial-guild-name');
+
+    // @ts-expect-error
+    if (guildIcon) guildIcon.src = testimonial.guild.icon;
+    if (guildName) guildName.textContent = testimonial.guild.name;
+
+    const authorNameElement = document.getElementById('testimonial-author-name');
+    const authorAvatarElement = document.getElementById('testimonial-author-avatar');
+    const authorPositionElement = document.getElementById('testimonial-author-position');
+
+    // @ts-expect-error
+    if (authorAvatarElement) authorAvatarElement.src = testimonial.author.avatar;
+    if (authorPositionElement) authorPositionElement.textContent = testimonial.author.position;
+    if (authorNameElement) authorNameElement.textContent = testimonial.author.name;
 };
 
 function startAutoSlide() {
@@ -52,11 +62,8 @@ function nextTestimonial() {
     renderTestimonial(index);
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('dom content loaded');
-
+setTimeout(() => {
     document.getElementById('prev-testimonial')?.addEventListener('click', () => {
-        console.log('a');
         prevTestimonial();
         resetAutoSlide();
     });
@@ -65,7 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
         nextTestimonial();
         resetAutoSlide();
     });
-});
 
-renderTestimonial(index);
-startAutoSlide();
+    renderTestimonial(index);
+    startAutoSlide();
+}, 5000);
