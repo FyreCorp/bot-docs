aeef063ee28684d4e267002992ff87693d06bf4a
Liam L <thefallenspirit@outlook.com>
2026-02-26 19:18:22 -0800

    Reduce space under headers

--- snippets/Header.jsx
+++ snippets/Header.jsx
@@ -6,7 +6,7 @@
  * @param {string} props.description
  */
 export const Header = ({ title, description }) => (
-    <div className="text-center mb-16">
+    <div className="text-center mb-12">
         <h2 className="mb-4 text-5xl font-bold text-gray-100">{title}</h2>
         <p className="text-xl text-gray-300">{description}</p>
     </div>
